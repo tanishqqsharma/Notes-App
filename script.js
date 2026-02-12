08b4b9fcbe0404aeec82bfedc2265368334791d0
tanishqqsharma <143253287+tanishqqsharma@users.noreply.github.com>
2026-02-13 01:18:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,21 @@ var input = document.getElementById("note-input");
 var NotesList = document.getElementById("notes-list");
 var addBtn = document.getElementById("add-btn");
 var Notes = document.getElementsByClassName("notes");
-var dlt = document.getElementsByClassName("dlt");
+var body    = document.body;                     
+var h1      = document.querySelector("h1");      
+var newNote = document.querySelector(".new-note"); 
+
+var Notes   = document.getElementsByClassName("notes"); 
+var dlt     = document.getElementsByClassName("dlt");   
+
+var themeBtn = document.querySelector(".theme-btn");    
+
 
 var i;
 for (i = 0; i < Notes.length; i++) {
     var span = document.createElement("span");
     span.innerText = "Delete";
-    span.classList = "dlt";
+    span.className = "dlt";
     Notes[i].appendChild(span);
 }
 
@@ -24,7 +32,7 @@ addBtn.addEventListener("click", addNote);
 function addNote() {
     if (title.value != "") {
         var div = document.createElement("div");
-        div.classList = "notes";
+        div.className = "notes";
         var h2 = document.createElement("h2");
         h2.innerText = title.value;
         div.appendChild(h2);
@@ -35,7 +43,7 @@ function addNote() {
 
         var span = document.createElement("span");
         span.innerText = "Delete";
-        span.classList = "dlt";
+        span.className = "dlt";
         div.appendChild(span);
 
         for (i = 0; i < dlt.length; i++) {
@@ -45,10 +53,35 @@ function addNote() {
         }
     }
     else {
-        prompt("Title is compulsory.");
+        alert("Title is compulsory.");
     }
     title.value = "";
     input.value = "";
 
 
+}
+
+
+themeBtn.addEventListener("click", changeTheme);
+
+function changeTheme() {
+    body.classList.toggle("dark");
+    h1.classList.toggle("dark");
+    newNote.classList.toggle("dark");
+    title.classList.toggle("dark");
+    input.classList.toggle("dark");
+    addBtn.classList.toggle("dark");
+    NotesList.classList.toggle("dark");
+    themeBtn.classList.toggle("dark");
+
+    for (let i = 0; i < Notes.length; i++) {
+        Notes[i].classList.toggle("dark");
+    }
+
+    for (let i = 0; i < dlt.length; i++) {
+        dlt[i].classList.toggle("dark");
+    }
+    themeBtn.textContent = document.body.classList.contains("dark")
+    ? "Light"
+    : "Dark";
 }
